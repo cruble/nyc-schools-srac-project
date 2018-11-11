273397c7c08648f0f2798d0b5edb9433136430e5
imamun93 <41834786+imamun93@users.noreply.github.com>
2018-11-11 15:54:07 -0500

    Add files via upload
    school_rating and sat_score api

---
+++ sat_score.py
@@ -0,0 +1,4 @@
+import requests
+r= requests.get('https://data.cityofnewyork.us/resource/734v-jeq5').json()
+sat_scores= r
+sat_scores
---
+++ school_rating.py
@@ -0,0 +1,4 @@
+import requests
+r= requests.get('https://data.cityofnewyork.us/resource/i3q5-gtjn').json()
+school_rating= r
+school_rating
