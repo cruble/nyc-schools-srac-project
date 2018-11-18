755f889a046d4f04f4af019bd0d0c7193d3876e8
Iftekher <mhshs.imamun@gmail.com>
2018-11-18 18:36:11 -0500

    two graphs and readme

--- dash_package/dashboard.py
+++ dash_package/dashboard.py
@@ -7,33 +7,24 @@ from dash_package.query import *
 import dash
 import plotly.graph_objs as go
 import plotly.plotly as py
-import pandas as pd 
+import pandas as pd
 
 from dash_package.mega_data import mega_list_2012, mega_list_2013, mega_list_2014, mega_list_2015, mega_list_2016, mega_list_2017
-
-# school_rating = [i.overall for i in db.session.query(Rating).all()]
-# school_sat= School=[i.math_avg for i in db.session.query(SchoolSAT).all()]
-
-# def sat_avg():
-#     name=[]
-#     math=[]
-#     for school in School.query.all():
-#         if school.sats[0].math_avg:
-#             name.append(school.name)
-#             math.append(school.sats[0].math_avg)
-#     return {'x': name, 'y': math, 'type': 'bar', 'name': 'Math Avgs'}
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('../Schools2013-3.csv', sep='\t')
+df = pd.read_csv('Schools2013.csv', sep='\t')
 
 
-app.layout = html.Div([
+app.layout = html.Div(children=[
+dcc.Tabs(id="tabs", children=[
+	dcc.Tab(id='satmath', label='Math AVG SAT',
+		children=[
     dcc.Graph(
-        id='life-exp-vs-gdp',
+        id='math-sat-avg',
         figure={
             'data': [
                 go.Scatter(
@@ -41,13 +32,14 @@ app.layout = html.Div([
                     y=df[df['Rating'] == i]['Attendance_Ratio'],
                     text=df[df['Rating'] == i]['Name'],
                     mode='markers',
+                    showlegend= False,
                     opacity=0.7,
                     marker={
                         'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
+                        'line': {'width': 0.5, 'color': 'black'}
                     },
                     name=str(i)
-                ) for i in df.Rating.unique()
+                ) for i in df.Rating[:].unique()
             ],
             'layout': go.Layout(
                 xaxis={'type': 'log', 'title': 'Math Average'},
@@ -58,31 +50,35 @@ app.layout = html.Div([
             )
         }
     )
-])
-
-
-
-
-# app.layout= html.Div('It\'s here...')
-# #working to here
-# app.layout = html.Div(
-#     children=[
-#     dcc.Tabs(id="tabs", children=[
-#         dcc.Tab(id='sat', label='sat for 2012',
-#             children=[
-#             dcc.Graph(figure=
-#             {'data': sat_avg(),
-#             'layout': {}})
-#             ]
-#         ),
-#         dcc.Tab(id='rating', label='School Ratings',
-#             children=[
-#             dcc.Graph(figure=
-#             {'data': overall_rating(),
-#             'layout': {}})
-#             ]
-#         )
-
-#         ])
-#     ]
-# )
+]),
+    dcc.Tab(id='satread', label='Reading AVG SAT',
+		children=[
+        dcc.Graph(
+            id='read-sat-avg',
+            figure={
+                'data': [
+                    go.Scatter(
+                        x=df[df['Rating'] == i]['Reading_Avg'],
+                        y=df[df['Rating'] == i]['Attendance_Ratio'],
+                        text=df[df['Rating'] == i]['Name'],
+                        mode='markers',
+                        showlegend= False,
+                        opacity=0.7,
+                        marker={
+                            'size': 15,
+                            'line': {'width': 0.5, 'color': 'white'}
+                        },
+                        name=str(i)
+                    ) for i in df.Rating.unique()
+                ],
+                'layout': go.Layout(
+                    xaxis={'type': 'log', 'title': 'Reading Average'},
+                    yaxis={'title': 'Attendance_Ratio'},
+                    margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                    legend={'x': 0, 'y': 1},
+                    hovermode='closest'
+                )
+            }
+        )
+    ])
+])])
