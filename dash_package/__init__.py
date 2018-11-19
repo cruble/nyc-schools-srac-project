d2c11c7c5090fc167e4a92010b560db230ee085d
Iftekher <mhshs.imamun@gmail.com>
2018-11-18 21:01:21 -0500

    adding pictures for read me

--- dash_package/__init__.py
+++ dash_package/__init__.py
@@ -10,10 +10,9 @@ server.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(server)
 
 app= dash.Dash(__name__, server= server, url_base_pathname= '/dashboard/')
-app2= dash.Dash(__name__, server= server, url_base_pathname= '/dashboard2/')
+# app2= dash.Dash(__name__, server= server, url_base_pathname= '/dashboard2/')
 
 from dash_package.models import *
 from dash_package.routes import *
 from dash_package.dashboard import *
 from dash_package.dashboard2 import *
-
